a8357573d20325fce4aee9c272d8f630c7e20c3e
Alexander Wende <alex.wende@gmail.com>
2018-12-28 23:57:48 +0100

    added missing type declarations

---
+++ dist/utils/index.d.ts
@@ -0,0 +1,5 @@
+export declare type Constructor<T = any> = {
+    new (...args: any[]): T;
+};
+export declare const isConstructor: (constructorFn: any) => constructorFn is Constructor<any>;
+export declare const isSymbol: (symbol: any) => symbol is Symbol;
