d35c8cd1b6702607ec4554739c9e6c419c4b57b7
Alexander Wende <alex.wende@gmail.com>
2019-07-28 19:34:05 +0200

    chore(release): 2.0.3

--- dist/cjs/injector.js
+++ dist/cjs/injector.js
@@ -8,8 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 var Injector_1;
+Object.defineProperty(exports, "__esModule", { value: true });
 "use strict";
 const index_js_1 = require("./annotations/index.js");
 const index_js_2 = require("./decorators/index.js");
